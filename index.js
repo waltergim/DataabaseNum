a79530b61b2db60b453fe2cb6eb41e8bfbc30325
waltergim <waltergim49@gmail.com>
2023-04-14 19:44:04 -0300

    14 casi terminado

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
 
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
  
 const Note = require('./models/note')
 
-
-app.use(cors())
 app.use(express.json())
-app.use(morgan("tiny"));
+app.use(cors())
 app.use(express.static("dist"));
 
  
@@ -25,9 +23,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
  
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'esta ruta no existe capo' })
-}
 
 
  
@@ -41,59 +36,97 @@ app.get("/api/notes", (req, res) => {
   
 })
 
-app.get("/api/info", (req, res) => {
-  const hora = new Date().getHours()
-  const Angeda = notes.reduce((counter, person) => {
-
-    if (person.name) counter++;
-
-
-
-    return counter
-
-  }, 0);
-  res.send(`La hora a la que se recibio la peticion fue : ${hora}hs la cantidad de angendas es ${Angeda}`)
-
-})
-
-app.get("/api/info/:id", (req, res) => {
- Note.findById(req.params.id).then(note=>{
-  res.json(note)
+app.get("/api/info/:id", (req, res,next) => {
+  Note.findById(req.params.id).then(note=>{
+   if(note){
+     res.json(note)
+   }else{
+     res.status(404).end()
+   }
+   }).catch(error => next(error))
+ 
  })
+
+// para arreglar
+app.get("/api/info", (req, res) => {
+  Note.find({})
+    .then((Note) => 
+    {res.send(
+        `<p>La cantidad de personas es 
+        ${Note.length}
+         </p><p>${new Date()}</p>`
+      );
+    })
+    .catch((error) => next(error));
+
 })
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
 
-  res.status(204).end("se elimino correctamente");
+app.delete("/api/notes/:id", (req, res,next) => {
+   Note.findByIdAndDelete(req.params.id)
+   .then(() =>{
+    res.status(204).end()
+   })
+   .catch(error=>next(error))
 });
 
  
 
 app.post("/api/notes", (req, res) => {
  const body = req.body
- if(body.name === undefined){
-  return res.status(400).json({
-    error: "Contenido ausente"
-  })
- }
-
+ 
 const  note = new Note({
-  name:body.name,
+  name: body.name,
   number: body.number,
-  important:body.important || false,
 })
+
 
 note.save().then(savedNote=>{
   res.json(savedNote)
-})
+}) 
 
 })
 
+app.put("/api/notes/:id", (request, response, next) => {
+   
+  const { name, number } = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'esta ruta no existe capo' })
+}
 
 
 app.use(unknownEndpoint)
+
+const Errores = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'mal formato de id' })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error)
+}
+
+app.use(Errores)
+
+
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+ 
 
 
 const url = process.env.MONGODB_URI
